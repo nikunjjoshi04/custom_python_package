978bc9b82f2eb295bd788414ef522d7e35d31a40
nikunjjoshi <nikunj.joshi@sculptsoft.com>
2024-03-16 01:17:31 +0530

    Two pkg in one repo

--- custom_python_package2/setup.py
+++ custom_python_package2/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="custom_python_package",
+    name="custom_python_package2",
     version="1.0.0",
     description="How to create custom python package",
     url="https://github.com/nikunjjoshi04/custom_python_package.git",
     author="Nikunj Joshi",
     author_email="nikunjjoshi04@gmail.com",
     license="MIT",
-    packages=["custom_python_package"],
+    packages=["custom_python_package2"],
     zip_safe=False,
 )
