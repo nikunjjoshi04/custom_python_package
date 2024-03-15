4a6047a2d8e112234fdf9f70bd1e7e95daf6756b
nikunjjoshi <nikunj.joshi@sculptsoft.com>
2024-03-16 00:58:00 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="custom_python_package",
+    version="1.0.0",
+    description="How to create custom python package",
+    url="https://github.com/nikunjjoshi04/custom_python_package.git",
+    author="Nikunj Joshi",
+    author_email="nikunjjoshi04@gmail.com",
+    license="MIT",
+    packages=["custom_python_package"],
+    zip_safe=False,
+)
